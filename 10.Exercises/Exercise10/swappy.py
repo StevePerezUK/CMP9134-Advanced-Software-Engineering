63de8af5a80c07d009113b17ab480a03ca5baf3f
Steven Perez <steve@altihex.com>
2023-03-02 11:02:06 +0000

    Exercise updates from function to OOP

--- 10.Exercises/Exercise10/swappy.py
+++ 10.Exercises/Exercise10/swappy.py
@@ -23,10 +23,30 @@ class GetInput(object):
         self._s1 = input("Please enter a string: ")
         self._s2 = input("Please enter another string: ")
    
+class CalcStrings(GetInput):
+
+    def __init__(self):
+        super().__init__()
+        self.get_input()
+        self._outstring = ""
+        self._outstring1 = ""
+        self._outstring2 = ""
 
 
+    def swap_strings(self):
+        self._outstring = "{0}{1} {2}{3}".format(self._s2[:2],self._s1[2:],self._s1[:2],self._s2[2:])
 
-    print("{0}{1} {2}{3}".format(string2[:2],string1[2:],string1[:2],string2[2:]))
 
-if __name__ == "__main__":
-    get_input()
+class SwapStrings(CalcStrings):
+
+    def __init__(self):
+        super().__init__()
+        self.swap_strings()
+        
+
+    def print(self):
+        print("{0}".format(self._outstring))            
+
+swap_strings = SwapStrings()
+swap_strings.print()
+
--- 10.Exercises/Exercise12/logic.py
+++ 10.Exercises/Exercise12/logic.py
@@ -1,30 +1,81 @@
 #!/usr/bin/env python3
 
-def get_input():
-    c_prompt= "Please enter an integer for {}: " 
-    rtn={}
-    rtn['x'] = int(input(c_prompt.format("x")))
-    rtn['y'] = int(input(c_prompt.format("y")))
-    rtn['z'] = int(input(c_prompt.format("z")))
+# def get_input():
+#     c_prompt= "Please enter an integer for {}: " 
+#     rtn={}
+#     rtn['x'] = int(input(c_prompt.format("x")))
+#     rtn['y'] = int(input(c_prompt.format("y")))
+#     rtn['z'] = int(input(c_prompt.format("z")))
 
-    return rtn
+#     return rtn
 
-def truth_decode(inputs):
-    statement1 = False
-    statement2 = False
-    statement3 = False
-    if (inputs['x'] < inputs['y']) or (inputs['y'] > inputs['z']):
-        statement1 = True    
-    print("\tx < y or y > z is \t\t{}".format(statement1))
+# def truth_decode(inputs):
+#     statement1 = False
+#     statement2 = False
+#     statement3 = False
+#     if (inputs['x'] < inputs['y']) or (inputs['y'] > inputs['z']):
+#         statement1 = True    
+#     print("\tx < y or y > z is \t\t{}".format(statement1))
     
-    if inputs['y'] != inputs['z']:
-        statement2 = True
-    print("\ty != z is \t\t\t{}".format(statement2))
+#     if inputs['y'] != inputs['z']:
+#         statement2 = True
+#     print("\ty != z is \t\t\t{}".format(statement2))
 
-    if (inputs['x'] >= inputs['z']) or (inputs['y'] > inputs['z']) and (inputs['x'] != inputs['y']):
-        statement3 = True
-    print("\tx >= z or y > z and x != y is \t{}".format(statement3))
+#     if (inputs['x'] >= inputs['z']) or (inputs['y'] > inputs['z']) and (inputs['x'] != inputs['y']):
+#         statement3 = True
+#     print("\tx >= z or y > z and x != y is \t{}".format(statement3))
 
 
-if __name__ == "__main__":
-    truth_decode(get_input())
+# if __name__ == "__main__":
+#     truth_decode(get_input())
+
+
+class GetInput(object):
+
+    def __init__(self):
+        self._numbers = {}
+
+    def get_input(self):
+        c_prompt= "Please enter an integer for {}: " 
+        self._numbers['x'] = int(input(c_prompt.format("x")))
+        self._numbers['y'] = int(input(c_prompt.format("y")))
+        self._numbers['z'] = int(input(c_prompt.format("z")))
+
+
+class CalculateLogic(GetInput):
+
+    def __init__(self):
+        super().__init__()
+        self.get_input()
+        self._statements = {
+            '1' : False,
+            '2' : False,
+            '3' : False
+        }        
+        
+    def truth_decode(self):
+        inp= self._numbers
+        if (inp['x'] < inp['y']) or (inp['y'] > inp['z']):
+            self._statements['1'] = True 
+        if inp['y'] != inp['z']:
+            self._statements['2'] = True
+        if (inp['x'] >= inp['z']) \
+            or  (inp['y'] >  inp['z']) \
+            and (inp['x'] != inp['y']):
+            self._statements['3'] = True
+    
+class ValidateStatements(CalculateLogic):
+
+    def __init__(self):
+        super().__init__()
+        self.truth_decode()
+
+    def print(self):
+        print("\tx < y or y > z is \t\t{}".format(self._statements['1']))    
+        print("\ty != z is \t\t\t{}".format(self._statements['2']))
+        print("\tx >= z or y > z and x != y is \t{}".format(self._statements['3']))
+
+validate_statements = ValidateStatements()
+validate_statements.print()
+
+
