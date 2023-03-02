686259a82be36d74e037015e366fb7ca6f983c0f
Steven Perez <steve@altihex.com>
2023-03-02 19:56:53 +0000

    IN-7 Squid Build

--- 10.Exercises/Exercise14/wierd_months.py
+++ 10.Exercises/Exercise14/wierd_months.py
@@ -1,75 +1,169 @@
 #!/usr/bin/env python3
 
-import sys
+# import sys
 
-def get_months():
+# def get_months():
 
-    start_months = [
-        'January', 
-        'March', 
-        'May',
-        'September', 
-        'December',
-    ]
-    remove_list = [
-     'May',
-     'September',
-     'October'   
-    ]
+#     start_months = [
+#         'January', 
+#         'March', 
+#         'May',
+#         'September', 
+#         'December',
+#     ]
+#     remove_list = [
+#      'May',
+#      'September',
+#      'October'   
+#     ]
 
-    input_month = ""
+#     input_month = ""
 
-    while True:
-        print("Current list of months")
-        print("{}".format(start_months))
-        input_month= input("Enter month: ")
-        if not valid_month(input_month,start_months):
-            print("{0} is not a valid month name - try again".format(input_month))
-            continue
-        x = start_months.append(input_month)
-        if len(start_months) == 12:
-            print("Current list of months")
-            print("{}".format(start_months))
-            break
-        else: 
-            print("Current month list is {} months".format(len(start_months)))
-            continue
+#     while True:
+#         print("Current list of months")
+#         print("{}".format(start_months))
+#         input_month= input("Enter month: ")
+#         if not valid_month(input_month,start_months):
+#             print("{0} is not a valid month name - try again".format(input_month))
+#             continue
+#         x = start_months.append(input_month)
+#         if len(start_months) == 12:
+#             print("Current list of months")
+#             print("{}".format(start_months))
+#             break
+#         else: 
+#             print("Current month list is {} months".format(len(start_months)))
+#             continue
     
-    for el in remove_list:
-        print("Removing {} from the month list".format(el))
-        start_months.remove(el)
+#     for el in remove_list:
+#         print("Removing {} from the month list".format(el))
+#         start_months.remove(el)
     
-    print("Final list of months = {}".format(start_months))
+#     print("Final list of months = {}".format(start_months))
 
-def valid_month(chk_month,input_list):
-    c_all_months = [
-        'January',
-        'February', 
-        'March', 
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November', 
-        'December'
-    ]
+# def valid_month(chk_month,input_list):
+#     c_all_months = [
+#         'January',
+#         'February', 
+#         'March', 
+#         'April',
+#         'May',
+#         'June',
+#         'July',
+#         'August',
+#         'September',
+#         'October',
+#         'November', 
+#         'December'
+#     ]
 
-    valid_month= False
-    for month in input_list:
-        if month == chk_month:
-            return valid_month
+#     valid_month= False
+#     for month in input_list:
+#         if month == chk_month:
+#             return valid_month
 
-    for month in c_all_months:
-        if month == chk_month:
-            valid_month= True
-            break
+#     for month in c_all_months:
+#         if month == chk_month:
+#             valid_month= True
+#             break
 
-    return valid_month
+#     return valid_month
 
 
 
-if __name__ == "__main__": 
-    get_months()
+# if __name__ == "__main__": 
+#     get_months()
+
+
+
+class Months(object):
+
+    def __init__(self):
+        self._start_months = [
+            'January', 
+            'March', 
+            'May',
+            'September', 
+            'December',
+        ]
+
+        
+        self._remove_list = [
+            'May',
+            'September',
+            'October'   
+        ]
+
+        self._all_months = [
+            'January',
+            'February', 
+            'March', 
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November', 
+            'December'
+        ]
+
+    def valid_month(self,chk_month,input_list):
+        valid_month= False
+        for month in input_list:
+            if month == chk_month:
+                return valid_month
+
+        for month in self._all_months:
+            if month == chk_month:
+                valid_month= True
+                break
+
+        return valid_month
+
+class UpdateMonths(Months):
+
+    def __init__(self):
+        super().__init__()
+        self._input_month = ""
+        self._update_months = self._start_months
+
+    def update_months(self):
+        while True:
+            try: 
+                print("Current list of months")
+                print("{}".format(self._update_months))
+                self._input_month= input("Enter month: ")
+                if not self.valid_month(self._input_month,self._update_months):
+                    print("{0} is not a valid month name - try again".format(self._input_month))
+                    continue
+                x = self._update_months.append(self._input_month)
+        
+                if len(self._update_months) == 12:
+                    print("Current list of months")
+                    print("{}".format(self._update_months))
+                    break
+                else: 
+                    print("Current month list is {} months".format(len(self._update_months)))
+                    continue
+            except Exception as e:
+                print(e)
+                continue
+            
+        
+    def remove_months(self):
+        for el in self._remove_list:
+            print("Removing {} from the month list".format(el))
+            self._update_months.remove(el)
+    
+    def final_print(self):
+        print("Final list of months = {}".format(self._update_months))
+
+um = UpdateMonths()
+um.update_months()
+um.remove_months()
+um.final_print()
+
+
+
--- 10.Exercises/Exercise15/hangman.py
+++ 10.Exercises/Exercise15/hangman.py
@@ -1,7 +1,89 @@
 #!/usr/bin/env python3
 
+# import random as r
+# import sys
+
+# def start_game():
+#     c_word_list = [
+#         'zoo',
+#         'cat',
+#         'dog',
+#         'tree',
+#         'house',
+#         'keyboard',
+#         'motorway',
+#         'mouse',
+#         'bird',
+#         'keys'
+#     ]
+
+#     index = r.randint(0,9)
+#     guess_word = c_word_list[index]
+#     print("Welcome to hangman")
+#     # - For debugging -- print("guess word = {}".format(guess_word))
+#     display_list = []
+#     init_display(display_list,guess_word)
+#     guess_count = 0 
+#     while True:
+#         guess= input("\nPlease Guess a letter of the word: ")
+#         if len(guess) > 1:
+#             print("Only one letter at a time Cowboy")
+#             continue
+#         response = match_letter(guess,guess_word,display_list)
+#         if not response['outcome']:
+#             guess_count += 1
+#         if guess_count == 7:
+#             print("You lose Cowboy")
+#             sys.exit(1)
+#         display_list = response['display_list']
+#         display_refresh(display_list)
+#         winner=True
+#         for el in display_list:
+#             if str(el) == str("_"):
+#                 winner=False       
+        
+#         if winner:
+#             print("\n\nYou WIN Cowboy\n")
+#             sys.exit(0) 
+
+# def display_refresh(display_list):
+#     for el in display_list:
+#         print("{} ".format(el),end='')
+
+# def match_letter(guess, guess_word,display_list):
+#     match= guess_word.find(guess)
+#     if match == -1:
+#         return {'outcome': False,'display_list' : display_list}
+#     display_list[match] = guess
+#     while True:
+#         match= guess_word.find(guess,match+1)
+#         if match == -1:
+#             return {'outcome' : True,'display_list' : display_list}
+#         else:
+#             display_list[match] = guess
+
+
+# def init_display(display_list,guess_word):
+#     print("Word = ",end='')
+#     for el in range(0,len(guess_word)):
+#         display_list.append('_')
+#         print("_ ",end='')
+#     print("")
+
+
+# if __name__ == "__main__":
+#     start_game()
+
+
 import random as r
 import sys
+
+
+class InitGame(object):
+    
+    def __init__(self):
+        self._word_list 
+
 
 def start_game():
     c_word_list = [
@@ -71,5 +153,6 @@ def init_display(display_list,guess_word):
     print("")
 
 
-if __name__ == "__main__":
-    start_game()
+
+
+
