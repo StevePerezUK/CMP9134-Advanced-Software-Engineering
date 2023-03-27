8c63bbf5834e663838c49a8de45cd63938f8af0e
Steven Perez <steve@altihex.com>
2023-03-27 11:14:42 +0100

    IN-7 Squid build

---
+++ 10.Exercises/16.Testing/test_is_prime.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+from is_prime import is_prime as ip
+import unittest
+
+
+class TestIsPrime(unittest.TestCase):
+    def test_simple_prime(self):
+        self.assertTrue(ip(1))
+
+    def test_simple_non_prime(self):
+        self.assertTrue(ip(2))
+
+    def test_simple_prime15(self):
+        self.assertFalse(ip(15))
+    
+    def test_simple_prime45(self):
+        self.assertFalse(ip(45))
+
+    def test_large_prime11587(self):
+        self.assertTrue(ip(11587))
+
+    def test_typeerror_1(self):
+        with self.assertRaises(TypeError):
+            ip(6.5)
+
+    def test_typeerror_2(self):
+        with self.assertRaises(TypeError):
+            ip('six')
+
+   
+    def test_typeerror_3(self):
+        with self.assertRaises(ValueError):
+            ip(-7)
+
+if __name__ == "__main__":
+    unittest.main()
+
